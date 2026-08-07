90ff93376ca02e4117a65f0a17f1c8023327c1b1
compliance-network <StopDataSelling@protonmail.com>
2026-08-07 12:02:36 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -654,7 +654,7 @@ document.addEventListener('click', async function(e) {
         e.preventDefault();
         e.stopPropagation();
 
-        const url = "https://supabase.co";
+        const url = ""https://qoffylaknlkadkryyfqq.supabase.co";
         const key = "sb_publishable_wKjWbotyB74WxGk1NVBm0w_I4Fox1Fn";
 
         if (window.supabase) {
